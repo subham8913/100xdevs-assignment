18ee87eb27154b33c499d3d79f69e13eee4a7090
Subham <subham6575@gmail.com>
2024-04-09 23:09:51 +0530

    week-3 middleware done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -15,7 +15,18 @@ let numberOfRequestsForUser = {};
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
-
+app.use((req,res,next)=>{
+  const userId=req.headers['user-id'];
+  if(!numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]=0;
+  }
+ 
+  if(numberOfRequestsForUser[userId]>=5){
+    return res.status(404).json({error: "Rate limit exceeded"});
+  }
+  numberOfRequestsForUser[userId]++;
+  next();
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
